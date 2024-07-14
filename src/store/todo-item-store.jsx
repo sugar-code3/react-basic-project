21689281a031a40017dea7b1b11b84da6beda23a
Dilip kumar <dilipkumarkd800@gmail.com>
2024-07-14 12:06:05 +0530

    context provider used

---
+++ src/store/todo-item-store.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+
+const TodoItemfromContext=createContext({
+    todoItem:[],
+    addNewItem:()=>{},
+    deleteItem:()=>{}
+})
+export default TodoItemfromContext;
